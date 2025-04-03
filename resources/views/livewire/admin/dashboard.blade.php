a4d0566e7c58315ced82f56288ac75e7467dc774
kamissvitoria <kmorssantos@gmail.com>
2025-04-03 13:58:17 -0300

    first commit

---
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+<div>
+  {{-- <h1>Adminstrador Logado</h1> 
+
+  <h4> {{ auth()->user()->email}} </h4>
+  <h4> {{ auth()->user()->idl}} </h4>
+  <h4> {{ auth()->user()->user_type}} </h4>
+  <h4> {{ auth()->user()->admin->cpf}} </h4>
+  <h4> {{ auth()->user()->admin->created_at}} </h4>
+ <h4> {{ auth()->user()->admin->id}} </h4> --}}
+</div>
